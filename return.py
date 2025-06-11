8a31544a16557c21572d5328e809a987cf4cd563
jiruneko <oohikaru310@gmail.com>
2025-06-11 13:48:32 +0900

    second commit

--- return.py
+++ return.py
@@ -4,3 +4,17 @@ def circle_area(r):
 
 a = circle_area(2.5)
 print("半径2.5の円の面積は", a)
+
+
+def is_positive(i):
+    if i > 0:
+        return True
+    else:
+        return False
+
+
+a = 10
+if is_positive(a) == True:
+    print("aの値は正です")
+else:
+    print("aの値は負またはゼロです")
